01a117f23e6fbf7c9574dc8f6ebd09d0b94fb224
Oleksandr <kozyrev.sania@gmail.com>
2025-09-21 18:40:50 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,47 @@
 'use strict';
 
 document.addEventListener('click', (e) => {
-  // write code here
+  if (e.target.className === 'wall') {
+    const spider = document.querySelector('.spider');
+    const wall = document.querySelector('.wall');
+
+    const coordsClick = {
+      x: e.clientX,
+      y: e.clientY,
+    };
+
+    const wallPosition = {
+      x: wall.offsetLeft,
+      y: wall.offsetTop,
+    };
+
+    const wallBorders = {
+      x: wall.clientLeft,
+      y: wall.clientTop,
+    };
+
+    const calcedCoords = {
+      x: coordsClick.x - wallPosition.x - wallBorders.x - spider.height / 2,
+      y: coordsClick.y - wallPosition.y - wallBorders.y - spider.height / 2,
+    };
+
+    if (calcedCoords.x < 0) {
+      calcedCoords.x = 0;
+    }
+
+    if (calcedCoords.x > wall.clientWidth - spider.width) {
+      calcedCoords.x = wall.clientWidth - spider.width;
+    }
+
+    if (calcedCoords.y < 0) {
+      calcedCoords.y = 0;
+    }
+
+    if (calcedCoords.y > wall.clientHeight - spider.width) {
+      calcedCoords.y = wall.clientHeight - spider.width;
+    }
+
+    spider.style.top = `${calcedCoords.y}px`;
+    spider.style.left = `${calcedCoords.x}px`;
+  }
 });
